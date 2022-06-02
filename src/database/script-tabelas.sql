e2a3a2359845b6afd66407180e17ef15a259d314
Sergio Daniel de Carvalho Duenas <99742467+daniel42p@users.noreply.github.com>
2022-06-02 16:27:40 -0300

    Add files via upload
    api

---
+++ src/database/script-tabelas.sql
@@ -0,0 +1,55 @@
+-- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
+-- Você precisa executar os comandos no banco de dados para criar as tabelas,
+-- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+
+/* para workbench - local - desenvolvimento */
+
+CREATE DATABASE artie;
+
+USE artie;
+
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+    fkGenero INT,
+    CNPJ VARCHAR(15),
+    telefone  VARCHAR(15),
+	email VARCHAR(50),
+	senha VARCHAR(100)
+);
+
+CREATE TABLE genero(
+idGenero INT PRIMARY KEY AUTO_INCREMENT,
+nomeGenero VARCHAR(45)
+);
+
+ALTER TABLE Usuario ADD FOREIGN KEY (fkGenero) REFERENCES genero(idGenero);
+
+
+
+/* para sql server - remoto - produção */
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY IDENTITY(1,1),
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50),
+);
+
+CREATE TABLE aviso (
+	id INT PRIMARY KEY IDENTITY(1,1),
+	titulo VARCHAR(100),
+    descricao VARCHAR(150),
+	fk_usuario INT FOREIGN KEY REFERENCES usuario(id)
+); 
+
+CREATE TABLE medida (
+	id INT PRIMARY KEY IDENTITY(1,1),
+	temperatura DECIMAL,
+	umidade DECIMAL,
+	momento DATETIME,
+	fk_aquario INT
+);
+
+
